566011838560b35df2e9d2db1c03cc2438ff7fac
pgenfer <pgenfer@gmail.com>
2023-04-17 16:09:13 +0200

    add action handlers

--- src-tauri/src/classifier_service.rs
+++ src-tauri/src/classifier_service.rs
@@ -1,18 +1,15 @@
-use std::collections::HashMap;
-
 use serde::{Serialize, Deserialize};
-use serde_json::Value;
 use strum_macros::Display;
 use ts_rs::TS;
 
-use crate::{action_handler::{ActionHandler, ActionReceiver, convert_and_handle_action}, repository::Repository, classifier::{Classifier}};
+use crate::{repository::Repository, classifier::Classifier};
 
 #[derive(TS, Serialize, Deserialize)]
 #[ts(export, rename_all="camelCase")]
 #[ts(export_to = "../src/bindings/edit-name-dto.ts")]
 #[serde(rename_all(deserialize="camelCase", serialize="camelCase"))]
 pub struct EditNameDto {
-    new_name: String
+    pub new_name: String
 }
 
 #[derive(Serialize, Deserialize, Display)]
@@ -31,17 +28,17 @@ pub enum ApplicationAction {
     ClassiffiersLoaded
 }
 
-// we need one file with constants, like
-// this will also be generated for redux to use a slice name
-pub const CLASSIFIER_DOMAIN: &str = "classifier";
+
+pub const APPLICATION_DOMAIN: &str = "application";
 
 pub struct ClassifierService {
     repository : Box<dyn Repository<Classifier> + Send + Sync>
 }
 impl<'a> ClassifierService {
     
-    pub fn new(classifier_repository: Box<dyn Repository<Classifier> + Send + Sync>) -> Self { 
-        Self { repository: classifier_repository } 
+    pub fn new(
+    classifier_repository: Box<dyn Repository<Classifier> + Send + Sync>) -> Self { 
+        Self { repository: classifier_repository }
     }
 
     pub async fn load_classifiers(&self) -> Vec<Classifier> {
@@ -49,7 +46,7 @@ impl<'a> ClassifierService {
         classifiers
     }
 
-    pub async fn create_new_classifier(&self, new_name: &str) -> Classifier { // TODO: use repository error
+    pub async fn create_new_classifier(&self, new_name: &str) -> Classifier {
         let id = uuid::Uuid::new_v4().to_string();
         let new_classifier = self.repository.insert(Classifier{
             _id: id.clone(), 
@@ -68,57 +65,4 @@ impl<'a> ClassifierService {
         let updated = self.repository.edit(&id, classifier).await;
         updated
     }
-
-    
 }
-
-impl ActionHandler<ClassifierAction> for ClassifierService {
-    fn handle_action(&self, action: ClassifierAction) -> Result<ClassifierAction, serde_json::Error> {
-        let response = match action {
-            ClassifierAction::RenameClassifier(data) => {
-                // self.update_classifier_name(&data.new_name);
-                ClassifierAction::ClassifierRenamed(data)
-            },
-            ClassifierAction::CancelClassifierRename =>
-                ClassifierAction::ClassifierRenameCanceled(
-                    EditNameDto { new_name: String::from("Old Classname") }
-                )
-            ,
-            _ => ClassifierAction::ClassifierRenameError
-        };
-        Ok(response)
-    }
-}
-
-impl ActionHandler<ApplicationAction> for ClassifierService {
-    fn handle_action(&self, action: ApplicationAction) -> Result<ApplicationAction, serde_json::Error> {
-        let response = match action {
-            _ => ApplicationAction::ClassiffiersLoaded
-        };
-        Ok(response)
-    }
-}
-
-impl ActionReceiver for ClassifierService {
-    fn receive_action(&self, json_action: serde_json::Value) -> Result<serde_json::Value, serde_json::Error> {
-        convert_and_handle_action::<ClassifierAction>(json_action, self)
-    }
-    fn domain(&self) -> &str { CLASSIFIER_DOMAIN}
-}
-
-// TODO: we need a way to register more than one action handler per service
-// for example, the classifier service must be able to handle classifier actions and application actions
-// We could implement the trait for every action and let the service register the action
-/*
-
-example:
-in service:
-    // we could use a closure here maybe?
-    // but the closure needs to be typed? not good...
-    handlers.register("classifier", |a| convert_and_handle_action::<ClassifierAction>(a, self))
- */
-
- pub fn test() {
-    let mut action_handlers = HashMap::<String, Box<fn(Value) -> Value> >::new();
-    
- }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,47 +3,48 @@
     windows_subsystem = "windows"
 )]
 
-use action_handler::{ActionReceiver, ActionHandler, convert_and_handle_action};
-//use bonsai_repository::BonsaiRepository;
+use classifier_action_handler::{handle_classifier_action, CLASSIFIER_DOMAIN};
 use surreal_repository::SurrealRepository;
-//use bonsaidb::{local::config::{StorageConfiguration, Builder}, core::permissions::Action};
-use classifier::Classifier;
 use surrealdb::{Surreal, engine::local::File};
 use tauri::{State};
 
 mod classifier_service;
-mod action_handler;
 mod classifier;
 mod value_objects;
 mod entity;
 mod bonsai_repository;
 mod surreal_repository;
 mod repository;
+mod classifier_action_handler;
 
 use std::{string::String, collections::HashMap};
-// use bonsaidb::{local::{Database, config::{StorageConfiguration, Builder}, Storage}, core::connection::StorageConnection};
 use serde::{Serialize, Deserialize};
 use serde_json::{Value};
 use std::string::ToString;
 
-use classifier_service::{ClassifierService, ClassifierAction, ApplicationAction};
+use classifier_service::{ClassifierService};
 
 #[tauri::command]
-fn ipc_message(message: IpcMessage, state: State<ApplicationState>, ) -> IpcMessage {
-    let message_handler = state.action_handlers.get(&*message.domain).unwrap(); 
-    let response = message_handler.receive_action(message.action).unwrap();
+fn ipc_message(message: IpcMessage, 
+    handler_state: State<ActionHandlerState>, 
+    service_state: State<ServiceState>) -> IpcMessage {
+    let classifier_service = &service_state.classifier;
+    let handler = handler_state.action_handlers.get(&message.domain).unwrap();
+    let response = handler(message.action, classifier_service);
     IpcMessage {
-        domain: message_handler.domain().to_string(),
+        domain: message.domain,
         action: response
     }
 }
 #[tokio::main]
 async fn main() {
     // create our application state
-    let state = ApplicationState::new().await;
+    let handler_state = ActionHandlerState::new().await;
+    let service_state = ServiceState::new().await;
     // setup and start Tauru
     tauri::Builder::default()
-        .manage(state)
+        .manage(handler_state)
+        .manage(service_state)
         .invoke_handler(tauri::generate_handler![ipc_message])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
@@ -55,41 +56,34 @@ struct IpcMessage {
     action: Value
 } 
 
-struct ApplicationState {
-    classifier_service: Box<ClassifierService>,
-    action_handlers: HashMap<String, &Box<dyn ActionReceiver + Send + Sync>>
+struct ServiceState {
+    classifier: ClassifierService
 }
 
-impl ApplicationState {
-    async fn new() -> ApplicationState { 
-        // setup surreal db, repository, service
+impl ServiceState {
+    async fn new() -> Self { 
         let surreal_db = Surreal::new::<File>("testdata/surreal/umlboard.db").await.unwrap();
         surreal_db.use_ns("umlboard_namespace").use_db("umlboard_database").await.unwrap();
-        let classifier_repository = Box::new(SurrealRepository::<Classifier>::new(Box::new(surreal_db), "classifiers"));
-        // let classifier_service = Box::new(ClassifierService::new(classifier_repository));
-        // let mut action_handlers = HashMap::<String, &Box<dyn ActionReceiver + Send + Sync>>::new();
-        // store everything in state
-        // action_handlers.insert(classifier_service.domain().to_string(), &*classifier_service);
-        let mut state = ApplicationState {
-            classifier_service: Box::new(ClassifierService::new(classifier_repository)),
-            action_handlers: HashMap::new()
-        };
-        let service = state.classifier_service;
-        state.action_handlers.insert(service.domain().to_string(), &(state.classifier_service));
-
-
-        let mut handlers = HashMap::new();
-
-        // handlers.insert("classifiers", Box::new(|a| convert_and_handle_action::<ClassifierAction>(a, &*classifier_service)));
-        handlers.insert("classifiers", |a, s| convert_and_handle_action::<ApplicationAction>(a, s));
-        // handlers.insert("classifiers", |a: ApplicationAction| classifier_service.handle_action(a));
-        let action = handlers.get("classifieres").unwrap();
-        
-        // action(ClassifierAction::CancelClassifierRename, classifier_service);
-        
-
-
-        state
+        let repository = Box::new(SurrealRepository::new(Box::new(surreal_db), "classifiers"));
+        let service = ClassifierService::new(repository);
+        Self { classifier: service } 
     }
 }
+
+struct ActionHandlerState {
+    action_handlers: HashMap<String, fn(Value, &ClassifierService) -> Value>
+}
+
+impl ActionHandlerState {
+    async fn new() -> Self { 
+        let mut action_handlers: HashMap<String, fn(Value, &ClassifierService) -> Value> = HashMap::new();
+        action_handlers.insert(CLASSIFIER_DOMAIN.to_owned(), handle_classifier_action);
+        Self {action_handlers}
+    }
+}
+
+
+
+
+
 
